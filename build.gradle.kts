a31a93be76c46fccb9e745b3d95df1703ddf39d3
shijiayao <32740499+shijiayao-main@users.noreply.github.com>
2023-06-01 16:43:07 +0800

    fix build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
 
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.2.0-alpha01").apply(false)
-    id("com.android.library").version("8.2.0-alpha01").apply(false)
+    id("com.android.application").version("8.0.2").apply(false)
+    id("com.android.library").version("8.0.2").apply(false)
     alias(libs.plugins.spotless).apply(false)
     kotlin("android").version("1.8.10").apply(false)
     kotlin("multiplatform").version("1.8.10").apply(false)
